c91580bdf91008b2aba52fd8039b0eb1fa85bf9a
Natasha Ridenour <nar000@ib12be-016.science.gc.ca>
2022-01-13 17:47:31 +0000

    Refactoring data parsing code

--- mysci.py
+++ mysci.py
@@ -1,8 +1,13 @@
-# Initialize my data variable
-data = {'date': [],
-        'time': [],
-        'tempout': []}
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2}
 
+# Data types for each column (only if non string)
+types = {'tempout': float}
+
+# Initialize my data variable
+data = {}
+for column in columns:
+    data[column] = []
 
 # Read the data file
 filename = "data/wxobs20170821.txt"
@@ -13,7 +18,9 @@ with open(filename,'r') as datafile:
  # Read and parse the rest of the file
  for line in datafile:
      split_line = line.split()
-     data['date'].append(split_line[0])
-     data['time'].append(split_line[1])
-     data['tempout'].append(float(split_line[2]))
+     for column in columns:
+         i = columns[column]
+         t = types.get(column, str)
+         value = t(split_line[i])
+         data[column].append(value)
 
