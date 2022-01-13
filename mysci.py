d9d16185a46d66801927316c30570bf8cec61c4c
Natasha Ridenour <nar000@ib12be-016.science.gc.ca>
2022-01-13 00:13:37 +0000

    Parsing file

--- mysci.py
+++ mysci.py
@@ -1,7 +1,14 @@
+# Initialize my data variable
+data = []
+
 # Read the data file
 filename = "data/wxobs20170821.txt"
 with open(filename,'r') as datafile:
-    data = datafile.read()
-
-datafile.close()
+    # Read the first three lines (header)
+ for _ in range(3):
+    datafile.readline()
+ # Read and parse the rest of the file
+ for line in datafile:
+     datum = line.split()
+     data.append(datum)
 
